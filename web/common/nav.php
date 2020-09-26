2df6f884f41082c43eadb8922034c83275d6844e
Mitchell Hudson <mitch.hudson18@gmail.com>
2020-09-26 15:06:26 -0600

    moved files and added links to other page.

--- web/common/nav.php
+++ web/common/nav.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-dark bg-dark">
-  <a class="navbar-brand">Hudsons Web</a>
+  <a class="navbar-brand" href="/index.php">Hudsons Web</a>
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
-        <a class="nav-link">Assignments</a>
+        <a class="nav-link" href="/view/assignments.php">Assignments</a>
       </li>
     </ul>
 </nav>
---
+++ web/view/assignments.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Hudson Assignments</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Mitchell Hudson">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
+</head>
+
+<body>
+    <nav>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/common/nav.php'; ?>
+    </nav>
+
+    <main>
+        <div class="jumbotron">
+            <h1 class="display-4">Coming Soon</h1>
+            <hr class="my-4">
+            <p>Assignments will be here once I have them and link them.</p>
+        </div>
+    </main>
+</body>
