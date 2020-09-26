d60e5cb43ee990a9d86cdca100923c002a60dfc9
Mitchell Hudson <mitch.hudson18@gmail.com>
2020-09-26 14:47:53 -0600

    Testing jumbotron look

--- web/common/nav.php
+++ web/common/nav.php
@@ -2,7 +2,7 @@
   <a class="navbar-brand">Hudsons Web</a>
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
-        <a class="nav-item">Assignments</a>
+        <a class="nav-link">Assignments</a>
       </li>
     </ul>
 </nav>
