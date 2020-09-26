2a69a2839422ba12c8dec2c09afc9c3f4b0433c6
Mitchell Hudson <mitch.hudson18@gmail.com>
2020-09-26 14:19:41 -0600

    Testing deploy

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Hudson Home</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Mitchell Hudson">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+
+<body>
+    <header>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/common/header.php'; ?>
+    </header>
+
+    <nav>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/common/nav.php'; ?>
+    </nav>
+</body>
