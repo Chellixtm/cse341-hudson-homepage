22ca7537267d10d6e76f00c448559888605094d7
Mitchell Hudson <mitch.hudson18@gmail.com>
2020-09-26 14:31:49 -0600

    testing collapse change

--- web/common/nav.php
+++ web/common/nav.php
@@ -1,10 +1,8 @@
 <nav class="navbar navbar-dark bg-dark">
   <a class="navbar-brand">Hudsons Web</a>
-  <div class="collapse navbar-collapse" id="navbarText">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
         <a class="nav-item">Assignments</a>
       </li>
     </ul>
-  </div>
 </nav>
